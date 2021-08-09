9d39c5d83b1be95f609990599bcd34f085d8a72a
cgorrino <cgorrino03@gmail.com>
2021-08-08 23:04:02 -0700

    Adding type defs for current models.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const {AuthenticationError} = require('apollo-server-express');
+const {Book, User} = require('../models');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+
+    },
+    Mutation: {
+        
+    }
+}
