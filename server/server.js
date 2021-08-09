71c70435a652db74ed62528f154288b7faad9512
cgorrino <cgorrino03@gmail.com>
2021-08-08 22:25:42 -0700

    Adding apollo server express and middleware to server.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+// Adding in Apollo Server for refactor
+const {ApolloServer} = require('apollo-server-express');
+// TODO: remove routes once they are replaced
 const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,9 +28,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
+// TODO remove after refactor
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
