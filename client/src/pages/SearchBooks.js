8cc55ac1aefe804284d71448b6fae5837a68684b
cgorrino <cgorrino03@gmail.com>
2021-08-11 00:53:44 -0700

    Adding required npm packages.

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,7 +4,7 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import {SAVE_BOOK} from '../utils/mutations';
 
 import Auth from '../utils/auth';
-//import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -15,6 +15,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, {error}] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -59,7 +61,7 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    const [saveBook, {error}] = useMutation(SAVE_BOOK);
+    
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
