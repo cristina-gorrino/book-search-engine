de42c29c11d2a34faf88b5df95f8bae56ca3405b
cgorrino <cgorrino03@gmail.com>
2021-08-11 15:56:25 -0700

    Fixing deploy bug. App not found.

--- server/server.js
+++ server/server.js
@@ -28,6 +28,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 // TODO remove after refactor
 app.use(routes);
 
