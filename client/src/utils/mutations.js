070140e1f0a04a7b0ebc8b030408127be010a820
cgorrino <cgorrino03@gmail.com>
2021-08-10 21:20:50 -0700

    Defining mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,49 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql``;
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+  `;
 
-export const ADD_USER = gql``;
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
-export const SAVE_BOOK = gql``;
+export const SAVE_BOOK = gql`
+mutation SaveBook ($input: savedBook) {
+    saveBook(input: $input) {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+}
+`;
 
-export const REMOVE_BOOK = gql``;
+export const REMOVE_BOOK = gql`
+mutation RemoveBook($bookId: String) {
+    removeBook(bookId: $bookId) {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+    }
+}
+`;
