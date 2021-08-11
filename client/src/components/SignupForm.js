ea56ba9b4e8ebd67fd234e7bde1f02e4a7d9cc1e
cgorrino <cgorrino03@gmail.com>
2021-08-11 15:49:35 -0700

    Quick change to deploy.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -37,7 +37,7 @@ const SignupForm = () => {
       Auth.login(data.addUser.token);
 
       console.log(data);
-      //Auth.login(token);
+      //Auth.login(token); trying again
     } catch (err) {
       console.error(err);
       setShowAlert(true);
